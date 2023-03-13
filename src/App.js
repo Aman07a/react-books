fa33ba772bc3aa2fac23c02190ab0c7338342afe
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-13 11:24:44 +0100

    Updating State

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("Need to add book with:", title);
+    // BAD CODE!
+    books.push({ id: 123, title: title });
+    console.log(books);
+    setBooks(books);
   };
 
   return (
